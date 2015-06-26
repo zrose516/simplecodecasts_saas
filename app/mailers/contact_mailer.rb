a5af4e5d0cf6b20c916d3f1063fc1ca21b318864
Zachary Rose <zrose516@gmail.com>
2015-06-26 03:34:38 +0000

    Added email address to contact_mailer

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,5 +1,5 @@
 class ContactMailer < ActionMailer::Base
-  default to: 'your@email_address.com'
+  default to: 'zrose516@gmail.com'
   def contact_email(name, email, body)
     @name = name
     @email = email
